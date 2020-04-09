f977e4d8cea16fba6fcdf245cab6d74c3b853f52
smlrkh1107 <krae7789230@gmail.com>
2020-04-09 22:08:28 +0900

    데이터클래스 생성, 한줄 XML 작성

---
+++ app/src/main/java/kun/hee/listveiw_20200409/datas/Room.kt
@@ -0,0 +1,11 @@
+package kun.hee.listveiw_20200409.datas
+
+class Room {
+    var price = 0
+    var address = "?구 ?동"
+    var floor = 1 //1~ 층수. 0층 : 반지하, -2.. 지하 2층
+    var desc = "방에 대한 설명"
+
+
+
+}
