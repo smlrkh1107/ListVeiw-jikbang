dcc6917ebb4e7e3f61c178ce1d370732918e7039
smlrkh1107 <krae7789230@gmail.com>
2020-04-13 19:51:40 +0900

    필기추가-

--- app/src/main/java/kun/hee/listveiw_20200409/adapters/RoomAdaper.kt
+++ app/src/main/java/kun/hee/listveiw_20200409/adapters/RoomAdaper.kt
@@ -10,17 +10,19 @@ import kun.hee.listveiw_20200409.datas.Room
 
 class RoomAdaper(context:Context, resId:Int, list:ArrayList<Room>) : ArrayAdapter<Room>(context,resId,list) {
 
-    val mContext = Context
+    val mContext = context
     val mList = list
     val inf = LayoutInflater.from(mContext)
 
+
+                                        //converView 함수가 nill일수도 있지 > inf정의
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        var tempRow = convertView
+        var tempRow = convertView  //바로 변수를 converView를 넣을 수 없음. 변수 새로 정의.
         if (tempRow == null) {
             tempRow = inf.inflate(R.layout.room_list_item, null)
         }
 
-        val row = tempRow!!
+        val row = tempRow!!  // !! 는 tempRow가 절대 null이 아니야!
         return row
     }
 
